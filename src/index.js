b50f9acc6cccdd8d9339c9ab5facbf0584b30a8b
Ingar Helgesen <ingar@i-h.no>
2020-05-11 11:03:49 +0200

    Rewritten logConfig() & date formatter

--- src/index.js
+++ src/index.js
@@ -2,60 +2,61 @@ const packPath = require('packpath').parent()
 const { join } = require('path')
 const syslog = require('syslog-client')
 const logLevelMapper = require('./lib/log-level-mapper')
+const getDateTime = require('./lib/get-date-time')
 
 const pkg = require(join(packPath, 'package.json'))
 
 // Store the options after configuration
-let remoteLogger
-const msgOpts = {}
+const loggerOptions = {
+  localLogger: console.log
+}
 
 /**
  * Logger configuration
- * @description Configure the logger for Papertrail and add message suffix/prefix
- * @param {object}  ptOpts           Options for Papertrail
- * @param {string}  ptOpts.disabled  If true; does not log to Papertrail
- * @param {string}  ptOpts.host      Hostname for the Papertrail server
- * @param {string}  ptOpts.port      Port for the Papertrail server
- * @param {string}  ptOpts.hostname  The name identifying name of the service (application / server)
- * @param {string}  prefix            A string that will be added in front of each log message (ex. UID for each run)
- * @param {string}  suffix            A string that will be added at the end of each log message
- * @returns {boolean}                 Returns true if logging to Papertrail
+ * @description Configure the logger and add message suffix/prefix
+ * @param {object}    [options]                           Options for logging
+ * @param {object}    [options.remote]                    Options for remote logging. If undefined; disables remote logging
+ * @param {string}    [options.remote.onlyInProd=true]    If true; only log to remote aggregator when NODE_ENV === 'production'
+ * @param {string}    [options.remote.host]               Hostname for the remote aggregator
+ * @param {string}    [options.remote.port]               Port for the remote aggregator
+ * @param {string}    [options.remote.serviceHostname]    The identificator of this service
+ * @param {string}    [options.remote.serviceAppname="default:"]   The identificator of this application (defaults to "default:" for consistency with Winston)
+ * @param {string}    [options.prefix]                    A string that will be added in front of each log message (ex. UID for each run)
+ * @param {string}    [options.suffix]                    A string that will be added at the end of each log message
+ * @param {function}  [options.localLogger=console.log]   Replace the local logger with a custom function (Default: console.log)
+ * @returns {void}
  */
-function logConfig (ptOpts, prefix, suffix) {
-  // TODO: Change to a single options parameter for easier management
-  // TODO: New logic for checking if Papertrail should be enabled.
-  if (ptOpts && typeof ptOpts !== 'object') logger('warn', ['Logger config', 'Wrong type on parameter "ptOpts <Object>"'])
-  if (process.env.NODE_ENV !== 'production') ptOpts.disabled = true
-
-  if (!ptOpts.disabled) {
-    ptOpts.host = ptOpts.host || process.env.PAPERTRAIL_HOST
-    ptOpts.port = ptOpts.port || process.env.PAPERTRAIL_PORT
-    ptOpts.hostname = ptOpts.hostname || process.env.PAPERTRAIL_HOSTNAME
-
-    if (ptOpts.host && typeof ptOpts.host !== 'string') logger('warn', ['Logger config', 'Wrong type on parameter "ptOpts.host <String>"'])
-    if (ptOpts.port && typeof ptOpts.port !== 'string') logger('warn', ['Logger config', 'Wrong type on parameter "ptOpts.port <String>"'])
-    if (ptOpts.hostname && typeof ptOpts.hostname !== 'string') logger('warn', ['Logger config', 'Wrong type on parameter "ptOpts.hostname <String>"'])
-  }
-
-  if (prefix && typeof prefix !== 'string') logger('warn', ['Logger config', 'Wrong type on parameter "prefix <String>"'])
-  if (suffix && typeof suffix !== 'string') logger('warn', ['Logger config', 'Wrong type on parameter "suffix <String>"'])
-  msgOpts.prefix = prefix
-  msgOpts.suffix = suffix
-  msgOpts.disabled = ptOpts.disabled
-
+function logConfig (options = {}) {
   if (
-    !ptOpts.disabled &&
-    ptOpts.host && // TODO: Make sure right type
-    ptOpts.port &&
-    ptOpts.hostname
+    options && typeof options === 'object' &&
+    options.remote && typeof options.remote === 'object'
   ) {
-    remoteLogger = syslog.createClient(ptOpts.host, {
-      port: ptOpts.port,
-      syslogHostname: ptOpts.hostname,
-      appName: 'default:', // Same as Winston for consistency
+    options.remote.host = options.remote.host || process.env.PAPERTRAIL_HOST
+    options.remote.port = options.remote.port || process.env.PAPERTRAIL_PORT
+    options.remote.serviceHostname = options.remote.serviceHostname || process.env.PAPERTRAIL_HOSTNAME
+    options.remote.serviceAppname = options.remote.serviceAppname || process.env.PAPERTRAIL_APPNAME || 'default:'
+
+    loggerOptions.remoteLogger = syslog.createClient(options.remote.host, {
+      port: options.remote.port,
+      syslogHostname: options.remote.hostname,
+      appName: options.remote.serviceAppname,
       transport: syslog.Transport.Udp,
       rfc3164: false // Use RFC5424
     })
+
+    // onlyInProd defaults to true
+    options.remote.onlyInProd = options.remote.onlyInProd === undefined
+
+    if (!(process.env.NODE_ENV !== 'production' && options.remote.onlyInProd)) {
+      loggerOptions.enabled = true
+    }
+  }
+
+  loggerOptions.prefix = typeof options.prefix === 'string' ? options.prefix : undefined
+  loggerOptions.suffix = typeof options.suffix === 'string' ? options.suffix : undefined
+
+  if (typeof options.localLogger === 'function') {
+    loggerOptions.localLogger = options.localLogger
   }
 }
 
@@ -64,14 +65,11 @@ function logConfig (ptOpts, prefix, suffix) {
  * @description This function will log a message with a level of severity
  * @param {string}          level     The severity of the message
  * @param {array<string>}   message   An array of strings which is joined by a hyphen in the log message
- * @returns {Promise<void>}
+ * @returns {void}
  */
 function logger (level, message) {
   // TODO: Create modules
-  const time = new Date()
-  const fDate = `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`
-  const fTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-
+  const { fDate, fTime } = getDateTime()
   const messageArray = Array.isArray(message) ? message : [message]
   let syslogSeverity = logLevelMapper(level)
 
@@ -81,19 +79,18 @@ function logger (level, message) {
     syslogSeverity = logLevelMapper(level)
   }
 
-  msgOpts.prefix && messageArray.unshift(msgOpts.prefix)
-  msgOpts.suffix && messageArray.push(msgOpts.suffix)
+  loggerOptions.prefix && messageArray.unshift(loggerOptions.prefix)
+  loggerOptions.suffix && messageArray.push(loggerOptions.suffix)
 
   const funcDetails = pkg && pkg.version ? `${pkg.name} - ${pkg.version}` : ''
   const logMessage = `${funcDetails}: ${messageArray.join(' - ')}`
   const remoteLogMessage = `${level.toUpperCase()} - ${logMessage}`
   const localLogMessage = `[ ${fDate} ${fTime} ] < ${level.toUpperCase()} >  ${logMessage}`
 
-  // Undefined if Papertrail logging is disabled or not in a production environment
-  if (remoteLogger) remoteLogger.log(remoteLogMessage, { severity: syslogSeverity })
+  if (loggerOptions.enabled) loggerOptions.remoteLogger.log(remoteLogMessage, { severity: syslogSeverity })
 
   // TODO: Option to pass in custom logger? (Ex. "context.log()" from Azure Functions)
-  console.log(localLogMessage)
+  loggerOptions.localLogger(localLogMessage)
 }
 
 module.exports = {
