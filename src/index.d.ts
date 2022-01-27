1bcbb226b0345c23a7fa311b0323a40fee871ef1
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-01-27 14:52:17 +0100

    No need to declare as a module

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,39 +1,37 @@
-declare module '@vtfk/logger' {
-  export type Levels = 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
+export type Levels = 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
 
-  export function logger (
-    level: Levels,
-    message: string[] | string
-  ): void
+export function logger (
+  level: Levels,
+  message: string[] | string
+): void
 
-  export function logConfig(
-    options?: LogConfigOptions
-  ): void
+export function logConfig(
+  options?: LogConfigOptions
+): void
 
-  interface LogConfigOptions {
-    remote?: {
-      disabled?: boolean
-      onlyInProd?: boolean
-      host?: string
-      token?: string
-    }
-    azure?: {
-      context?: {
-        invocationId?: string
-        log?: {
-          error: () => void
-          warn: () => void
-          info: () => void
-          verbose: () => void
-        }
+interface LogConfigOptions {
+  remote?: {
+    disabled?: boolean
+    onlyInProd?: boolean
+    host?: string
+    token?: string
+  }
+  azure?: {
+    context?: {
+      invocationId?: string
+      log?: {
+        error: () => void
+        warn: () => void
+        info: () => void
+        verbose: () => void
       }
-      excludeInvocationId?: boolean
     }
-    prefix?: string
-    suffix?: string
-    localLogger?: (message: any) => void
-    error?: {
-      useMessage?: boolean
-    }
+    excludeInvocationId?: boolean
+  }
+  prefix?: string
+  suffix?: string
+  localLogger?: (message: any) => void
+  error?: {
+    useMessage?: boolean
   }
 }
