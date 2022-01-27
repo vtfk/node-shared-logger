1bb5212abe9a57389d12ba4bb48cdd9a533735b7
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-01-27 14:52:44 +0100

    Export `logger` as async

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ export type Levels = 'silly' | 'debug' | 'verbose' | 'info' | 'warn' | 'error'
 export function logger (
   level: Levels,
   message: string[] | string
-): void
+): Promise<void>
 
 export function logConfig(
   options?: LogConfigOptions
